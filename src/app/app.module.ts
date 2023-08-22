55155eb9328cac5ecf70bf24241ce4d350eee15e
Yiğit Tanyel <yigittanyel@trex.com.tr>
2023-08-22 10:04:50 +0300

    NgRx State Management applied.

---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { StoreModule } from '@ngrx/store';
+import { counterReducer } from './NgRxTrial/state/counter.reducer';
+import { MyCounterComponent } from './NgRxTrial/my-counter/my-counter.component';
+@NgModule({
+  declarations: [
+    AppComponent,
+    MyCounterComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    StoreModule.forRoot({ count: counterReducer })
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
